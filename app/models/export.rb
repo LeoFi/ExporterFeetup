37b8256cdafa800c69009d6b0502e966d7f08fd1
Bui The Hoa <buithehoa@gmail.com>
2018-08-21 11:08:18 +0700

    Strip time string.

--- app/models/export.rb
+++ app/models/export.rb
@@ -6,7 +6,7 @@ class Export < ApplicationRecord
   def self.available_exports(time = nil)
     # time ||= Time.zone.now.strftime('%H:00')
     tz = TZInfo::Timezone.get("Europe/Berlin")
-    time ||= tz.utc_to_local(Time.now.utc).strftime("%k:00")
+    time ||= tz.utc_to_local(Time.now.utc).strftime("%k:00").strip
 
     puts "[INFO] ExportJob: #{where(time: time).count} scheduled export(s) found at #{time}"
     where(time: time)
